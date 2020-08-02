a14d01a8556932c9cad7db1c5058496e578b46a2
Sergey <zugurysus@gmail.com>
2020-08-02 17:20:47 +1000

    The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,21 @@
             @endforeach
         </div>
     </div>
+
+    @if(auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-4">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea required name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>
+                    </div>
+                    <button class="btn btn-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center mt-5">Please <a href="{{ route('login') }}">sign in</a> to participate in this descussion.</p>
+    @endif
 </div>
 @endsection
